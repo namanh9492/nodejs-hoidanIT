15f6d8d64eebef9f91dcb7245a7812f821e6efd5
namanh9492 <102205391+namanh9492@users.noreply.github.com>
2026-01-08 01:53:08 +0700

    tsc setup

--- app.ts
+++ app.ts
@@ -1,9 +1,9 @@
-const express = require("express");
+// const express = require("express");
+import express from "express";
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  // return "Hello World!";
   res.send("Hello world!");
 });
 
