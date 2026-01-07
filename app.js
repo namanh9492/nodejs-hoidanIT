04565ddcadce5fe3c0afdd778935f0a6d385472e
namanh9492 <102205391+namanh9492@users.noreply.github.com>
2026-01-07 15:54:19 +0700

    running first web Hello World by app.get(req,res)

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hello world!");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  // return "Hello World!";
+  res.send("Hello world!");
+});
+
+app.get("/namanh", (req, res) => {
+  res.send("Hello Nam Anh");
+});
+
+app.listen(port, () => {
+  console.log(`running at ${port}`);
+});
