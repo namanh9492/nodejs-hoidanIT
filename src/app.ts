8c178b9d06b88f7af6035fd4526733d65efdad71
namanh9492 <102205391+namanh9492@users.noreply.github.com>
2026-01-28 16:37:45 +0700

    update create user page + update user controller and make user services to get user info

--- src/app.ts
+++ src/app.ts
@@ -6,13 +6,20 @@ import webRoutes from "./routes/web";
 const app = express();
 const port = process.env.port;
 
+// config view engine
 app.set("view engine", "ejs");
 // app.set("views", "./src/views");
 app.set("views", __dirname + "/views");
 
-webRoutes(app);
+// config req.body to get info from user (ex: from create user page)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// config static files (img - css - js)
 app.use(express.static("public"));
+
+// config routes
+webRoutes(app);
 
 app.listen(port, () => {
   console.log(`env port: ${process.env.port}`);
---
+++ src/services/user.services.ts
@@ -0,0 +1,5 @@
+const handleCreateUser = (fullName: String, email: String, address: String) => {
+  console.log("checked user info");
+};
+
+export { handleCreateUser };
